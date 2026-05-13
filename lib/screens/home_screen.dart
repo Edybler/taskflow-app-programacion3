492669fead2540f6c50f27f34effde17043088bb
Edybler <Edy000036@gmail.com>
2026-05-13 15:22:48 -0600

    Eliminacion de líneas innecesarias y comentarios irrelevantes en el archivo home_screen.dart.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,8 +4,6 @@ import '../data_structures/hash_table.dart';
 import '../models/task_model.dart';
 import '../services/api_service.dart';
 import '../data_structures/linked_list.dart';
-
-// 🔥 NUEVAS PANTALLAS
 import 'analysis_screen.dart';
 import 'queue_screen.dart';
 import 'stack_screen.dart';
